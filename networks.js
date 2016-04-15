2d8ce5f2a00787b341c9d4eccfc08af89c9d8d17
Esotericizm <esotericizm88@gmail.com>
2016-04-15 13:15:07 +1000

    DigiByte

--- networks.js
+++ networks.js
@@ -6,32 +6,35 @@ var hex = function(hex) {
 
 exports.livenet = {
   name: 'livenet',
-  magic: hex('f9beb4d9'),
-  addressVersion: 0x00,
+  magic: hex('fac3b6da'),
+  addressVersion: 0x1e,
   privKeyVersion: 128,
   P2SHVersion: 5,
   hkeyPublicVersion: 0x0488b21e,
   hkeyPrivateVersion: 0x0488ade4,
   genesisBlock: {
-    hash: hex('6FE28C0AB6F1B372C1A6A246AE63F74F931E8365E15A089C68D6190000000000'),
-    merkle_root: hex('3BA3EDFD7A7B12B27AC72C3E67768F617FC81BC3888A51323A9FB8AA4B1E5E4A'),
+    hash: hex('96841e6ecc8dc9643aaddfb6fcd616e08f0777c87b508f1c9fb35e461bea9774'),
+    merkle_root: hex('72ddd9496b004221ed0557358846d9248ecd4c440ebd28ed901efc18757d0fad'),
     height: 0,
-    nonce: 2083236893,
+    nonce: 2447652,
     version: 1,
     prev_hash: buffertools.fill(new Buffer(32), 0),
-    timestamp: 1231006505,
+    timestamp: 1389388394,
     bits: 486604799,
   },
   dnsSeeds: [
-    'seed.bitcoin.sipa.be',
-    'dnsseed.bluematt.me',
-    'dnsseed.bitcoin.dashjr.org',
-    'seed.bitcoinstats.com',
-    'seed.bitnodes.io',
-    'bitseed.xf2.org'
+    '74.208.230.160',
+    '216.250.125.121',
+    '195.130.216.149',
+    '96.18.212.86',
+    '188.226.239.21',
+    '54.201.183.106',
+    '213.81.142.62'
   ],
-  defaultClientPort: 8333
+    defaultClientPort: 12024,
+    peerVersion: 70003
 };
+
 
 exports.mainnet = exports.livenet;
 
